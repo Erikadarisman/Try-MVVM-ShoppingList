1f84f4cef5a7b67991f3d93def2d4d9ae269ec52
Erik <erikadarisman@gmail.com>
2020-03-12 14:33:43 +0700

    initiate in shopping activity

--- app/src/main/java/com/kadarisman/trymvvmshoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/kadarisman/trymvvmshoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -5,12 +5,18 @@ import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.kadarisman.trymvvmshoppinglist.R
+import com.kadarisman.trymvvmshoppinglist.data.db.ShoppingDatabase
+import com.kadarisman.trymvvmshoppinglist.data.repositories.ShoppingRepository
 
 class ShoppingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+        val database = ShoppingDatabase(this)
+        val repository = ShoppingRepository(database)
+        val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this).get(ShoppingViewModel::class.java)
     }
