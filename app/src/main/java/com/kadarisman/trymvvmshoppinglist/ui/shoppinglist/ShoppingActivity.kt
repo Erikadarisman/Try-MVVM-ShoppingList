cfdf832262fcf6611458c1c51210f435634dd8ee
Erik <erikadarisman@gmail.com>
2020-03-13 14:58:39 +0700

    intiate class by class n create function

--- app/src/main/java/com/kadarisman/trymvvmshoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/kadarisman/trymvvmshoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -2,11 +2,17 @@ package com.kadarisman.trymvvmshoppinglist.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kadarisman.trymvvmshoppinglist.R
 import com.kadarisman.trymvvmshoppinglist.data.db.ShoppingDatabase
+import com.kadarisman.trymvvmshoppinglist.data.db.entities.ShoppingItem
 import com.kadarisman.trymvvmshoppinglist.data.repositories.ShoppingRepository
+import com.kadarisman.trymvvmshoppinglist.other.ShoppingItemAdapter
+import kotlinx.android.synthetic.main.activity_shopping.*
 
 class ShoppingActivity : AppCompatActivity() {
 
@@ -19,5 +25,25 @@ class ShoppingActivity : AppCompatActivity() {
         val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
+
+        val adapter = ShoppingItemAdapter(listOf(), viewModel)
+
+        rvShoppingItems.layoutManager = LinearLayoutManager(this)
+        rvShoppingItems.adapter = adapter
+
+        viewModel.getAllShoppingItems().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+        fab.setOnClickListener{
+            AddShoppingItemDialog(this,
+                object : AddDialogListener{
+                    override fun onAddButtonClicked(item: ShoppingItem) {
+                        viewModel.upsert(item)
+                    }
+                }).show()
+        }
     }
+
 }
